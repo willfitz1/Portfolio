f48e93da1029b1d47d397a82e9872090a9c1d459
William Fitzgerald <willfitzy03@gmail.com>
2022-09-19 11:50:01 -0400

    added details

--- portfolio/src/routes/About.js
+++ portfolio/src/routes/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import HeroImg2 from '../components/HeroImg2'
 
 const About = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg2 heading="ABOUT." text="Im a friendly Front-End Developer" />
       <Footer />
     </div>
   )

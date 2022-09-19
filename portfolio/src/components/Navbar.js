0fd35a212f02bcecc4f4f976bd0cd7a5bb7ddf90
William Fitzgerald <willfitzy03@gmail.com>
2022-09-19 00:46:51 -0400

    added font

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
-      <ul>
+      <ul className="nav-menu">
         <li>
           <Link to="/">Home</Link>
         </li>

c8b60a5bb58c11bac35dc134e90e4de69406b363
William Fitzgerald <willfitzy03@gmail.com>
2022-09-19 11:07:32 -0400

    put nav and footer on all pages

--- portfolio/src/routes/About.js
+++ portfolio/src/routes/About.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
 }
 
 export default About
--- portfolio/src/routes/Project.js
+++ portfolio/src/routes/Project.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const Project = () => {
-  return <div>Project</div>
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
 }
 
 export default Project

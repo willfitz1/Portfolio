e3e249a019a54fb84b8300df37defd5a57da0feb
William Fitzgerald <willfitzy03@gmail.com>
2022-09-19 00:41:01 -0400

    started nav bar

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import './NavbarStyles.css'
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className="header">
+      <Link to="/">
+        <h1>Portfolio</h1>
+      </Link>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/project">Project</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
